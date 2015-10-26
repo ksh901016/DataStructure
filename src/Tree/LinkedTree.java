d8fc5201c9f1dec2b0d9ca1b93fb27229da1c00b
KangSungHyun <ksh901016@naver.com>
2015-10-26 22:23:43 +0900

    이진트리 순회

---
+++ src/Tree/LinkedTree.java
@@ -0,0 +1,40 @@
+package Tree;
+
+public class LinkedTree {
+	private TreeNode root;
+	
+	public TreeNode makeBT(TreeNode bt1, Object data, TreeNode bt2){
+		TreeNode root = new TreeNode();
+		root.data = data;
+		root.left = bt1;
+		root.right = bt2;
+		return root;
+	}
+	
+	//전위 순회
+	public void preorder(TreeNode root){ 
+		if(root != null){
+			System.out.printf("%c", root.data);
+			preorder(root.left);
+			preorder(root.right);
+		}
+	}
+	//중위 순회
+	public void inorder(TreeNode root){
+		if(root != null){
+			inorder(root.left);
+			System.out.printf("%c", root.data);
+			inorder(root.right);
+		}
+	}
+	//후위 순회
+	public void postorder(TreeNode root){
+		if(root != null){
+			postorder(root.left);
+			postorder(root.right);
+			System.out.printf("%c", root.data);
+		}
+		
+	}
+
+}
---
+++ src/Tree/TreeNode.java
@@ -0,0 +1,8 @@
+package Tree;
+
+public class TreeNode {
+	Object data;
+	TreeNode left;
+	TreeNode right;
+
+}
